b67781cfc96ee534bf078805ffc7b4b8bb0c9daf
Your Name <you@example.com>
2021-07-10 15:01:06 -0500

    (Added Slices.py)

---
+++ Slices.py
@@ -0,0 +1,12 @@
+my_string = "Test Automation"
+word_test = my_string[:4]
+print(word_test)
+
+my_string = 'Test Automation'
+second_word = my_string[-10:]
+print(second_word)
+
+my_list=[10,50,20,25,30]
+sliced_list= my_list[1:3]
+print(sliced_list)
+
